0e6feadaa9f18e82f2e7fa0cb83e78dadef9abe3
DhanushM12 <dhanushmukhi@gmail.com>
2023-07-18 20:00:18 +0530

    added code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import UseEffect from './hooks/UseEffect';
+import UseMemo from './hooks/UseMemo';
+import UseRef from './hooks/UseRef';
+import UseState from './hooks/UseState';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hooks</h1>
+      <UseState />
+      <br />
+      <UseEffect />
+      <br />
+      <UseRef />
+      <br />
+      <UseMemo />
     </div>
   );
 }
